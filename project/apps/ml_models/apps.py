49916c56ce87417c101abc7bccbc84576b80d51a
@deathdater <aditya.juet@gmail.com>
2026-05-13 00:41:38 +0530

    Scaffold Django project structure and app modules

---
+++ project/apps/ml_models/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MlModelsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "project.apps.ml_models"
---
+++ project/settings/__init__.py
@@ -0,0 +1,7 @@
+"""Settings package.
+
+Set DJANGO_SETTINGS_MODULE to one of:
+- project.settings.local
+- project.settings.dev
+- project.settings.prod
+"""
